4953241993b38da1c4a0546f8678e7cee09f1e8f
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-18 15:46:52 -0300

    object

---
+++ 1- Tipos de Dados/object.js
@@ -0,0 +1,15 @@
+/* 
+   * Object
+      *Objeto
+      *Propriedades / Atributos
+      *Funcionalidades / Métodos
+
+   {propriedades: "valor"}
+*/
+console.log ({
+   name: "Guilherme",
+   idade: 17,
+   andar: function() {
+      console.log('Andar')
+   }
+})
