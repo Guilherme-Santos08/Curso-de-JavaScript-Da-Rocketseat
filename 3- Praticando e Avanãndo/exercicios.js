b4d4812d6a74f7f74688375df9eaaad05a1bd9db
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-18 17:09:11 -0300

    exercicios

---
+++ 3- Praticando e Avanãndo/exercicios.js
@@ -0,0 +1,30 @@
+// 1. Declare uma variável de nome wight
+// let weight
+
+// // 2. Que tipo de dado é a variável acima?
+// console.log(typeof weight)
+
+/* 4. Declare uma variável e atribua valores para cada um dos dados:
+*/
+
+//  name ="Guilherme"
+//  age = 17
+//  stars = 5
+//  isSubscribed = true
+
+/* 4. A variável student abaixo é de qe tipo de dados?
+   
+   4.1 Atribua a ela as mesma propriedades e valores do exercício 3.
+
+   4.2 Mostre no console a seguinete mensagem:
+   <name> de idade <age> pesa <weight> kg
+*/
+
+
+let student  = {
+   name: "Guilherme",
+    age: 17,
+    stars: 5,
+    isSubscribed: true
+}
+console.log(student)
