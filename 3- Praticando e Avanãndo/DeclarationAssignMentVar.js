a33c2ef2822de3822e913685b07c20bc1d5c0ee8
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-18 16:39:43 -0300

    agrupamento de declarações

---
+++ 3- Praticando e Avanãndo/DeclarationAssignMentVar.js
@@ -0,0 +1,19 @@
+// Variáveis e tipos de dados
+// Declaração or declaration
+
+   var name
+
+   // assignment or atriuição de valores
+   name = "mike"
+
+   // que tipo de dado foi colocado na variável
+
+   // console.log(typeof name)
+
+   // agrupamento de declarações
+   let age, isHuman
+
+   age = 18
+   isHuman = true
+
+   console.log(name, age, isHuman)
