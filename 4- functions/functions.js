d2edd5cabb47301ba32db3dee65d89568da4c947
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-19 15:59:56 -0300

    functions

---
+++ 4- functions/functions.js
@@ -0,0 +1,15 @@
+// Criar um aplicativo de frases motivacionais
+
+// Declaratio - declaração da função
+// function statement
+function createPhrases() {
+   console.log('Estuda é muito bom')
+   console.log('Paciência e persistência')
+   console.log('Revisão é mãe do aprendizado')
+}
+
+//exutar a função
+// execute run, call, invoke
+createPhrases()
+
+console.log('fim do programa')
