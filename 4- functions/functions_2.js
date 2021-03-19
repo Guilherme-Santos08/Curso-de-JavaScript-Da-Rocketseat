dd6289b85707fe7b06830036939cdb95688d4505
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-19 16:22:21 -0300

    Function 2

--- 4- functions/functions_2.js
+++ 4- functions/functions_2.js
@@ -3,7 +3,16 @@
 
 // parâmetros (parameters)
 const sum = function(number1, number2) { 
-   console.log(number1 + number2)
+   let total = (number1 + number2) // <== Recomendado
+   // total = (number1 + number2) <== Não recomendado
+   return total
 }
 
-sum(3, 3) // arguments - argumentos
+// sum(3, 3) // arguments - argumentos
+
+let number1 = 34
+let number2 = 25
+
+// console.log(`o número 1 é ${number1}`)
+// console.log(`o número 2 é ${number2}`)
+console.log(`o soma é ${sum(number1, number2)}`)
