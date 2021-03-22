930de379d23fc94ab4b0f311b0f17c82c0429f88
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-22 15:50:11 -0300

    falsyEtruthy

---
+++ 6- Espressoes e Operadores/11- falsyEtruthy.js
@@ -0,0 +1,37 @@
+/*
+  Type conversion(typecasting) vs Type coersion
+*/
+
+console.log(Number('9') + 5)
+
+/*
+  Falsy
+  Quando um valor é considerado false em    contextos onde um booleano é obrigatório (   condicionais e loops 
+  
+  false
+  0
+  -0
+  ""
+  null
+  undefined
+  NaN
+*/
+
+console.log( 0 ? 'verdadeiro' : 'falso')
+
+/*
+  TRUTHY
+  Quando um valor é considerado truem em  contextos onde um booleano é obrigatório ( condicionais e loops )
+
+  true
+  {}
+  []
+  1
+  3.23
+  "0"
+  "false"
+  -1
+  Infinity
+  -Infinity
+*/
+console.log( "0" ? 'verdadeiro' : 'falso')
