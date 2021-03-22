da2e9f0c046c18ba7a266f8fbfca5dbca69448ea
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-22 15:55:24 -0300

    precedenciaDosOperadores

---
+++ 6- Espressoes e Operadores/12- precedenciaDosOperadores.js
@@ -0,0 +1,16 @@
+/* Operator precedence
+    Precedência de operadores
+
+* grouping                        ( )
+* negação e incremento            ! ++ --
+* multiplicação e divisão         * /
+* adição e subtração              + -
+* relacional                      < <= > >=
+* igualdade                       == != === !==
+*                                 &&
+*                                 ||
+*                                 ?:
+*                                 = += -= *= %=
+*/
+
+console.log( 3 > 2 && 2 > 1)
