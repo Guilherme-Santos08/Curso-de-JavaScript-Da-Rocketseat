b8f5e18425d5f7552c79ecf93cbaab80c9ed3e94
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-22 14:21:28 -0300

    new

---
+++ 6- Espressoes e Operadores/2- new.js
@@ -0,0 +1,15 @@
+/*
+  new
+
+  * left-hand-side expression
+  * crar um novo objeto
+*/
+
+let name = new String ('Dz')
+name.surName = 'Script'
+
+let age = new Number(17)
+console.log(name, age)
+
+let date = new Date('2021/03/22')
+console.log(date)
