baeb8d18b2dc2e70ab5ec8dd3673cf6fb475dde0
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-03-23 11:08:29 -0300

    throwEtryCatch

--- 7- Condicionais e controle de fluxo/4- throwEtryCatch.js
+++ 7- Condicionais e controle de fluxo/4- throwEtryCatch.js
@@ -2,16 +2,16 @@
 
 function sayMyName(name = '') {
   if (name === ''){
-      throw 'Nome é obrigatório'
+      throw 'Nome é obrigatório' // throw
   }
 
   console.log('depois do erro')
 }
 
 // try...catch
-try {
+try { // try
   sayMyName()
-} catch(e) {
+} catch(e) { // catch
     console.log(e)
 }
 
